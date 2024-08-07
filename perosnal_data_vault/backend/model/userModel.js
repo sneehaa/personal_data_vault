1a8065fde28ebb665070e73e5838db1872093adf
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-07 09:15:16 +0545

    updated userModel.js

---
+++ perosnal_data_vault/backend/model/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String },
+  data: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Data' }]
+});
+
+userSchema.pre('save', async function(next) {
+  const user = this;
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
