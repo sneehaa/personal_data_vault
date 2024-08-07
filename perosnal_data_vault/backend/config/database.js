48163a52ebcf2e9cc46cbed42281538b3e7d3ba3
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-07 12:47:52 +0545

    removed unnecessary

--- perosnal_data_vault/backend/config/database.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const dbUrl = 'mongodb://localhost:27017/personal-data-vault';
-const dbName = 'personal-data-vault';
-
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
