772a5a0d477f874180235b491973f846c96320a5
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-07 12:46:24 +0545

    created VerifyEmail.jsx

---
+++ perosnal_data_vault/frontend/src/pages/VerifyEmail.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import axios from "axios";
+import success from "../assets/images/success.png";
+import styles from "../styles/module.css";
+import { Fragment } from "react";
+
+const VerifyEmail = () => {
+	const [validUrl, setValidUrl] = useState(true);
+	const param = useParams();
+
+	useEffect(() => {
+		const verifyEmailUrl = async () => {
+			try {
+				// Adjusted URL to match the backend port
+				const url = `http://localhost:5500/api/user/${param.id}/verify/${param.token}`;
+				const { data } = await axios.get(url);
+				console.log(data);
+				setValidUrl(true);
+			} catch (error) {
+				console.log(error);
+				setValidUrl(false);
+			}
+		};
+		verifyEmailUrl();
+	}, [param]);
+
+	return (
+		<Fragment>
+			{validUrl ? (
+				<div className={styles.container}>
+					<img src={success} alt="success_img" className={styles.success_img} />
+					<h1>Email verified successfully</h1>
+					<Link to="/login">
+						<button className={styles.green_btn}>Login</button>
+					</Link>
+				</div>
+			) : (
+				<h1>404 Not Found</h1>
+			)}
+		</Fragment>
+	);
+};
+
+export default VerifyEmail;
