8028e13cbf14901c1207af2ec2be7683792b8685
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-07 12:44:28 +0545

    updated app.js

--- perosnal_data_vault/frontend/src/App.js
+++ perosnal_data_vault/frontend/src/App.js
@@ -1,33 +1,54 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
-import authReducer from './reducers/authReducer';
-import dataReducer from './reducers/dataReducer';
-import Login from './Login';
-import Register from './Register';
-import Profile from './Profile';
-import DataVault from './DataVault';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import UserProfile from "./pages/UserProfile";
+import Login from "./pages/Login.jsx";
+import Navbar from "./components/Navbar.jsx";
+import UserRoutes from "./protectedRoutes/UserRoutes.jsx";
+import VerifyEmail from "./pages/VerifyEmail.jsx";
 
-const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    data: dataReducer,
-  },
-});
+
+
 
 function App() {
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("user"));
+    if (userData && userData.isAdmin) {
+      setIsAdmin(true);
+    }
+  }, []);
+
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/data-vault" element={<DataVault />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <Router>
+      <Navbar isAdmin={isAdmin} />
+      <ToastContainer />
+      <Routes>
+
+  
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/user/:id/verify/:token" element={<VerifyEmail />} />
+        <Route element={<UserRoutes />}>
+          <Route path="/profile" element={<h1>Profile</h1>} />
+        </Route>
+{/* 
+        {isAdmin && (
+          <Route element={<AdminRoutes />}>
+            <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            <Route path="/admin/users" element={<AdminUsers/>} />
+            <Route path="/admin/bookings" element={<AdminBookings/>} />
+            <Route path='/admin/edit/:id' element={<AdminEditHotel/>} />
+          </Route>
+        )} */}
+
+      </Routes>
+    </Router>
   );
 }
 
