14a48af4557bbbf06e3d529ef50afb0f48678c37
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-07 12:48:49 +0545

    updated index.js

--- perosnal_data_vault/backend/index.js
+++ perosnal_data_vault/backend/index.js
@@ -1,14 +1,58 @@
+// importing
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cloudinary = require('cloudinary');
+const acceptMultimedia = require('connect-multiparty');
+const connectDB = require('./database/database');
+
+
+// Making express app
 const app = express();
 
-// Enable CORS
-app.use(cors());
+// dotenv config
+dotenv.config();
 
-// Add a route for the root URL
-app.get('/', (req, res) => {
-  res.send('Welcome to my server!');
+// cloudinary config
+
+          
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+app.use(acceptMultimedia())
+
+// cors config to accept request from frontend
+const corsOptions = {
+    origin: true,
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions))
+
+// mongodb connection
+connectDB();
+
+// Accepting json data
+app.use(express.json());
+
+// creating test route
+app.get("/test", (req,res) => {
+    res.status(200).send("Hello");
+})
+
+
+// user route
+app.use('/api/user', require('./routes/userRoutes'))
+
+// defining port
+const PORT = process.env.PORT;
+// run the server
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
+// exporting app
+module.exports = app;
