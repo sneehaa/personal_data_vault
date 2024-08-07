4b84ca4ab128ed6302e191c6af7fa523688c966b
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-07 09:17:54 +0545

    created authReducer.js

---
+++ perosnal_data_vault/frontend/src/reducers/authReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+  user: null,
+  token: null,
+  error: null
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'LOGIN_SUCCESS':
+      return { ...state, user: action.payload.user, token: action.payload.token, error: null };
+    case 'LOGIN_FAILURE':
+      return { ...state, error: action.payload };
+    case 'REGISTER_SUCCESS':
+      return { ...state, user: action.payload.user, token: action.payload.token, error: null };
+    case 'REGISTER_FAILURE':
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
