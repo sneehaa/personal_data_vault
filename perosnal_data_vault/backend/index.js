74fe50b47c72943301e55efb09ec0f6043404546
Snehaa <94774019+sneehaa@users.noreply.github.com>
2024-08-10 11:33:33 +0545

    updated index.js

--- perosnal_data_vault/backend/index.js
+++ perosnal_data_vault/backend/index.js
@@ -5,7 +5,9 @@ const cors = require('cors');
 const cloudinary = require('cloudinary');
 const acceptMultimedia = require('connect-multiparty');
 const connectDB = require('./database/database');
-
+const session = require('express-session'); 
+const MongoStore = require('connect-mongo'); 
+const upload = require('./multer_config');
 
 // Making express app
 const app = express();
@@ -14,15 +16,13 @@ const app = express();
 dotenv.config();
 
 // cloudinary config
-
-          
 cloudinary.config({ 
   cloud_name: process.env.CLOUD_NAME, 
   api_key: process.env.CLOUDINARY_API_KEY, 
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-app.use(acceptMultimedia())
+app.use(acceptMultimedia());
 app.use(express.urlencoded({ extended: true }));
 
 // cors config to accept request from frontend
@@ -31,7 +31,7 @@ const corsOptions = {
     credentials: true,
     optionSuccessStatus: 200
 };
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 // mongodb connection
 connectDB();
@@ -39,26 +39,44 @@ connectDB();
 // Accepting json data
 app.use(express.json());
 
+// Configure session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET, 
+    resave: false,
+    saveUninitialized: false, 
+    store: MongoStore.create({ 
+        mongoUrl: process.env.DB_URL, 
+        ttl: 14 * 24 * 60 * 60
+    }),
+    cookie: {
+        secure: process.env.NODE_ENV === 'production', 
+        httpOnly: true,
+        sameSite: 'strict', 
+        maxAge: 14 * 24 * 60 * 60 * 1000 
+    }
+}));
+
 
 // creating test route
-app.get("/test", (req,res) => {
+app.get("/test", (req, res) => {
     res.status(200).send("Hello");
-})
-
+});
 
 // user route
-app.use('/api/user', require('./routes/userRoutes'))
+app.use('/api/user', require('./routes/userRoutes'));
 
-app.use('/api/data', require('./routes/dataRoutes'))
+app.use('/api/data', require('./routes/dataRoutes'));
 
 app.use('/uploads', express.static('uploads'));
+
+
 
 // defining port
 const PORT = process.env.PORT;
 // run the server
-app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 // exporting app
 module.exports = app;
